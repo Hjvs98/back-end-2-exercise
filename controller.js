b03ba3cb5d22cfbc8010b2ec48e6baa3a19118d2
Hunter Saunders <hjvs98@gmail.com>
2022-05-21 12:36:07 -0600

    first commit

--- controller.js
+++ controller.js
@@ -1,7 +1,36 @@
-const getHouses
+const house = require("./db.json");
+const houseId = 4;
 
-const deleteHouse
+module.exports = {
+  getHouses: (req, res) => res.status(200).send(house),
 
-const createHouse
+  deleteHouse: (req, res) => {
+    let index = house.findIndex((elem) => elem.id === +req.params.id);
+    house.splice(index, 1);
+    res.status(200).send(house);
+  },
 
-const updateHouse
+  createHouse: (req, res) => {
+    let { address, price, imageURL } = req.body;
+    let newHouse = {
+      id: houseId,
+      address,
+      price,
+      imageURL,
+    };
+    house.push(newHouse);
+    res.status(200).send(house);
+    houseId++;
+  },
+
+  // updateHouse: (req, res) => {
+  //   let { id } = req.params;
+  //   let { type } = req.body;
+  //   let index = house.findIndex((elem) => +elem.id === +id);
+  //   if (type === "plus") {
+  //     res.status(200).send(house[index].price + 10000);
+  //   } else if (type === "minus") {
+  //     res.status(200).send(house[index].price + 10000);
+  //   }
+  // },
+};
--- index.js
+++ index.js
@@ -5,5 +5,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const houseController = require("./controller.js");
+
+app.get("/api/houses", houseController.getHouses);
+app.post("/api/houses", houseController.createHouse);
+app.put("/api/houses/:id", houseController.updateHouse);
+app.delete("/api/houses/:id", houseController.deleteHouse);
+
 let port = 3005;
 app.listen(port, () => console.log(`I'll be right by your side 'til ${port}`));
